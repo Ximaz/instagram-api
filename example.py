92b5d18258528def689f308c66583bec26c05d32
Ximaz <malo.durand@epitech.eu>
2024-04-06 17:09:12 +0200

    adding the example.py

---
+++ example.py
@@ -0,0 +1,14 @@
+import apigram
+
+
+def main():
+    target = "sunnyrayyxo"
+    api = apigram.ApiGram(username=target, proxies=None)
+    recent_posts = api.fetch_posts()
+    print(recent_posts)
+    data = recent_posts["data"]["user"]["edge_owner_to_timeline_media"]["edges"]
+    for post in data:
+        print(post["node"]["display_url"])
+
+if __name__ == "__main__":
+    main()
